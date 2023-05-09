a61247d9fa1be82eed946f4c6d701e7bc93325d3
DamianMlM <leonel.mariscal5040@alumnos.udg.mx>
2023-05-08 21:55:53 -0600

    Opciones de menu desplegable completo

--- paginas/menu_principal.php
+++ paginas/menu_principal.php
@@ -10,6 +10,7 @@
     <title>Streamscape</title>
     <link rel="stylesheet" href="../css/style_general.css">
     <link rel="icon" href="../imagenes/streamscape_icono.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
 </head>
 <body>
@@ -27,15 +28,47 @@
                     <label for="btn-menu" class="icon-menu" ><img src="../imagenes/menu_icon1.png" alt="menu-icon" class="icon-menu"></label>
                 </div>
             </li>
-        </ul>
-        <div class="container-menu">
-            <div class="cont-menu" >
-                <nav>
-                    <a href="">Plataformas</a>
+        </ul> 
+                <!-- <a href="">Plataformas</a>
                     <a href="">Series</a>
                     <a href="">Usuarios</a>
                     <a href="">Busqueda Personalizada</a>
-                    <a href="../index.php">Cerrar sesión</a>
+                    <a href="../index.php">Cerrar sesión</a> -->
+
+        <div class="container-menu">
+            <div class="cont-menu" >
+                <nav>
+                    <div class="sidenav">
+                        <button class="dropdown-btn">Plataformas 
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-container">
+                            <a href="#">Crear</a>
+                            <a href="#">Eliminar</a>
+                            <a href="#">Editar</a>
+                            <a href="#">Vista general</a>
+                        </div>
+                        <button class="dropdown-btn">Series 
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-container">
+                            <a href="#">Crear</a>
+                            <a href="#">Eliminar</a>
+                            <a href="#">Editar</a>
+                            <a href="#">Vista general</a>
+                        </div>
+                        <button class="dropdown-btn">Usuarios 
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-container">
+                        <a href="#">Crear</a>
+                            <a href="#">Eliminar</a>
+                            <a href="#">Editar</a>
+                            <a href="#">Vista general</a>
+                        </div>
+                        <a href="#contact">Busqueda Personalizada</a>
+                        <a href="#contact">Cerrar sesión</a>
+                    </div>
                 </nav>
                 <label for="btn-menu" class="icon-cerrar"><img src="../imagenes/cerrar_icon1.png" alt=""></label>
             </div>
@@ -75,9 +108,27 @@
 
 
 <script>
+     /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+        } else {
+        dropdownContent.style.display = "block";
+        }
+    });
+    }                   
+
+
     const btnMenu = document.getElementById("btn-menu");
     const containerMenu = document.querySelector(".container-menu");
     const contMenu = document.querySelector(".cont-menu");
+    // const sideNav = document.querySelector(".sidenav");
   
     btnMenu.addEventListener("click", () => {
       containerMenu.classList.toggle("visible");
