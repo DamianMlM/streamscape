453648a5d46f6363755ee50590bf82912ca93c6e
Damian Mariscal <98064779+DamianMlM@users.noreply.github.com>
2023-05-14 03:22:16 -0600

    Archivos de conexion eliminados

--- php/bd.php
+++
@@ -1,6 +0,0 @@
-<?php
-        //Insertamos el código PHP donde nos conectamos a la base de datos *******************************
-        // require_once "conexion.php";
-        require_once "conexionxamp.php";
-
-?>
